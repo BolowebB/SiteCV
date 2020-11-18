c7044e67a6f08f1ca314b392430df04f8433259c
Bolo <bertrand.bolosier@gmail.com>
2020-11-18 09:49:47 +0100

    first commit

---
+++ menu/menu.php
@@ -0,0 +1,24 @@
+<head>
+
+</head>
+<body>
+
+      <nav>
+<div class="etat">
+            <ul class="menu">
+                <li id="comp"><a href="#competence">compétence </a></li>
+                <li id="dip"><a href="#diplome_formation">diplômes<br>et<br>formations</a></li>
+                <li id="exp"><a href="#">expérience<br> professionelles</a></li>
+                <li id="cont"><a href="#">contact</a></li>
+
+            </ul>
+    <a href="#"><img class="icone" src="image/ico_phone.png" alt=""></a>
+    <a href="#"><img class="icone un" src="image/ico_email.png" alt=""></a>
+    <a href="#"><img class="icone deux" src="image/ico_portefolio.png" alt=""></a>
+
+</div>
+
+        </nav>
+
+</body>
+</html>
